ee9719532f677ffcff589b4426acf7aeeacace2f
Liam Bolan <AQ231688@aquinas.ac.uk>
2024-07-02 13:44:07 +0100

    Add project files.

---
+++ Solitaire major project/SolitaireGame.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Solitaire_major_project
+{
+    public partial class SolitaireGame : Form
+    {
+        List<Card> CardsOnScreen = new List<Card>();
+        List<acebox> aceboxes = new List<acebox>();
+        public SolitaireGame()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void SolitaireGame_Load(object sender, EventArgs e)
+        {
+            
+            deck deck = new deck();
+            deck.Shuffle();
+
+            boxdeck boxdeck = new boxdeck();
+            
+            for (int i = 24; i > 0; i--)
+            {
+                
+                Card c = deck.Draw();
+                if (i == 24) { c.flip(); }
+                c.Location = new Point(55+1*i, 53);
+                c.Parent = this;
+
+            }
+            for (int i = 7; i > 0; i--)
+            {
+         
+                Card c = deck.Draw();
+                if (i == 7) { c.flip(); }
+                c.Location = new Point(975, 300 + 10 * i);
+                c.Parent = this;
+
+            }
+            for(int i = 6; i > 0; i--)
+            {
+                Card c = deck.Draw();
+                if (i == 6) { c.flip(); }
+                c.Location = new Point(825, 300 + 10 * i);
+                c.Parent = this;
+            }
+            for (int i = 5; i > 0; i--)
+            {
+                Card c = deck.Draw();
+                if (i == 5) { c.flip(); }
+                c.Location = new Point(675, 300 + 10 * i);
+                c.Parent = this;
+            }
+            for (int i = 4; i > 0; i--)
+            {
+                Card c = deck.Draw();
+                if (i == 4) { c.flip(); }
+                c.Location = new Point(525, 300 + 10 * i);
+                c.Parent = this;
+            }
+            for (int i = 3; i > 0; i--)
+            {
+                Card c = deck.Draw();
+                if (i == 3) { c.flip(); }
+                c.Location = new Point(375, 300 + 10 * i);
+                c.Parent = this;
+            }
+            for (int i = 2; i > 0; i--)
+            {
+                Card c = deck.Draw();
+                if (i == 2) { c.flip(); }
+                c.Location = new Point(225, 300 + 10 * i);
+                c.Parent = this;
+            }
+            for (int i = 1; i > 0; i--)
+            {
+                Card c = deck.Draw();
+                if (i == 1) { c.flip(); }
+                c.Location = new Point(75, 300 + 10 * i);
+                c.Parent = this;
+            }
+            acebox Acebox = new acebox();
+            for(int i = 0; i < 4; i++)
+            {
+                acebox a = boxdeck.Draw();
+                a.Location = new Point(75, 800 + 20 * i);
+                a.Parent = this;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
